5d35149e741ae19ab56d9fa4a72d46fc5e762271
mikamp116 <mikamardanyan@gmail.com>
2020-04-07 14:48:00 +0200

    Small fixes

--- deteccion_orb.py
+++ deteccion_orb.py
@@ -31,7 +31,7 @@ def ordenar(list):
 def load2(dir):
     cur_dir = os.path.abspath(os.curdir)
     files = ordenar(os.listdir(cur_dir + '/' + dir))
-    return [cv2.imread('train/' + file, 0) for file in files]
+    return [cv2.imread(dir + '/' + file, 0) for file in files]
 
 def softLoad():
     train = []
@@ -81,31 +81,34 @@ class Match:
         return self.des_angle
 
 train = load2('train')
-orb = cv2.ORB_create(nfeatures=100,nlevels=30)
+
+orb = cv2.ORB_create(nfeatures=100, scaleFactor=1.3, nlevels=4)
 
 """Usaremos una estructura de datos tipo FLANN para almacenar los descriptores"""
 
 FLANN_INDEX_LSH = 6
 index_params= dict(algorithm = FLANN_INDEX_LSH, table_number = 6,key_size = 3,multi_probe_level = 1)
-search_params = dict(checks=-1) # Maximum leafs to visit when searching for neighbours.
+search_params = dict(checks=-1)
 flann = cv2.FlannBasedMatcher(index_params,search_params)
 
 """Almacenamos la información de los puntos de interés del entrenamiento en match_table"""
 match_table = []
+
 # Bucle de entrenamiento
 for image in train:
     kps, des = orb.detectAndCompute(image,None)
-    image_det = cv2.drawKeypoints(image, kps, None, color=(255,0,255), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # image_det = cv2.drawKeypoints(image, kps, None, color=(255,0,255), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     image_match = [Match(calculateModule(k.pt),calculateAngleToCentre(k.pt),k.size, k.angle) for k in kps]
     match_table.append(image_match)
     flann.add([des])
 
-test = cv2.imread('test/test17.jpg')
-# plt.imshow(test)
+# Test
+
+test = cv2.imread('test/test11.jpg')
 test_kps, test_des = orb.detectAndCompute(test, None)
-sh = cv2.drawKeypoints(test, test_kps, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-plt.imshow(sh)
-plt.show()
+# sh = cv2.drawKeypoints(test, test_kps, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# plt.imshow(sh)
+# plt.show()
 
 results = flann.knnMatch(test_des, k=3)
 
@@ -115,12 +118,12 @@ for r in results:
     for m in r:
         match = match_table[m.imgIdx][m.trainIdx]
         m_test = test_kps[m.queryIdx]
-        trns = (match.getScale() / m_test.size ) * match.getModule()
+        # trns = (match.getScale() / m_test.size ) * match.getModule()
         trns2 = (m_test.size / match.getScale()) * match.getModule()
         angle = match.getKpAngle() + match.getDesAngle() - m_test.angle
         x = int((m_test.pt[0] + (trns2 * math.cos(angle))) / 10)
         y = int((m_test.pt[1] - (trns2 * math.sin(angle))) / 10)
-        if x>0 and x<matriz_votacion.shape[1] and y>0 and y<matriz_votacion.shape[0]:
+        if 0 < x < matriz_votacion.shape[1] and 0 < y < matriz_votacion.shape[0]:
             matriz_votacion[y,x] += 1
 
 
